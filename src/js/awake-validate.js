250284be72033faf3cba93ba88914a9589dc8030
alexwakewetail <alex@wetail.se>
2021-10-27 22:13:39 +0300

    fixed logic for validation

--- src/js/awake-validate.js
+++ src/js/awake-validate.js
@@ -1,76 +1,143 @@
 class AwakeValidate {
-    constructor(selector, options) {
-        this.$form = document.querySelector(selector);
-        if (this.$form) {
-            this.$inputs = this.$form.querySelectorAll('*[required=""]');
-        }
-        this.isValid = false;
-        this.$errors = [];
-        this.#assign();
+  constructor ( selector, options ) {
+    this.$form = document.querySelector( selector );
+    if ( this.$form ) {
+      this.$requiredElements = this.$form.querySelectorAll( '*[required=""]' );
     }
+    this.isValid = false;
+    this.$errors = [];
+    this.#assign();
+  }
 
-    #assign() {
-        if (this.$form) {
-            this.submitHandler = this.submitHandler.bind(this);
-            this.changeHandler = this.changeHandler.bind(this);
-            this.inputHandler = this.inputHandler.bind(this);
-            this.$form.addEventListener('submit', this.submitHandler)
-            this.$inputs.forEach(input => {
-                if (input.type === 'checkbox' || input.type === 'radio') {
-                    input.addEventListener('change', this.changeHandler)
-                }else {
-                    input.addEventListener('input', this.inputHandler)
+  #assign () {
+    if ( this.$form ) {
+      this.submitHandler = this.submitHandler.bind( this );
+      this.changeHandler = this.changeHandler.bind( this );
+      this.inputHandler = this.inputHandler.bind( this );
+      this.$form.addEventListener( 'submit', this.submitHandler );
+      if ( this.$requiredElements ) {
+        this.$requiredElements.forEach( input => {
+          if ( input.type === 'checkbox' || input.type === 'radio' ) {
+            input.addEventListener( 'change', this.changeHandler );
+          } else {
+            input.addEventListener( 'input', this.inputHandler );
+          }
+        } );
+      }
+
+    }
+  }
+
+  changeHandler ( e ) {
+
+  }
+
+  /**
+   * Email validation helper
+   * @param email
+   * @returns {boolean}
+   */
+  validateEmail ( email ) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test( String( email ).toLowerCase() );
+  }
+
+  checkValue ( name, value, target, type ) {
+      let error = 0;
+      let validate = true;
+      switch ( type ) {
+          case 'email':
+              if (value && !this.validateEmail(value)) {
+                  error++;
+                  validate = false
+              }
+
+              if (!value && !this.validateEmail(value)) {
+                  error++;
+                  validate = true
+              }
+              break;
+          default:
+              if (!value) {
+                  error++;
+                  validate = true
+              }
+      }
+
+      if (error > 0 ) {
+          return {
+              errorCount: error,
+              name: name,
+              validate: validate
+          }
+      } else {
+          return false;
+      }
+
+
+  }
+
+  inputHandler ( e ) {
+    let target = e.target;
+    const { name, value, type } = target;
+
+    const errors = this.checkValue( name, value, target, type );
+
+    if (errors && Object.keys(errors).length > 0) {
+        if (this.$errors.length) {
+            const err = this.$errors.find(item => item.name === errors.name);
+            const err2 = this.$errors.find(item => item.name === errors.name && item.validate !== errors.validate);
+
+            if ( !err ) {
+                this.$errors.push(errors);
+                if (!target.classList.contains('invalid')) {
+                    target.classList.add('invalid')
                 }
-            })
-        }
-    }
+            }
 
-    changeHandler(e) {
+            if (err2) {
+                this.$errors.find(item => {
+                    if (item.name === errors.name && item.validate !== errors.validate) {
+                        item.validate = errors.validate;
+                    }
+                })
+            }
 
-    }
-
-    inputHandler(e) {
-        let target = e.target;
-        const {name, value, type} = target;
-
-        if (!value) {
-            this.$errors.push({
-                name: name,
-            })
         }else {
-            const removeErrors = this.$errors.filter(item => item.name !== name);
-            this.$errors = [ ...removeErrors];
-            target.nextElementSibling.remove();
-        }
-
-        const existName = this.$errors.find(item => item.name === name);
-
-        if (existName) {
-            const err = document.createElement('span');
-            err.classList.add('error');
-            err.textContent = `${name} is required field and should not be empty`;
-            target.parentNode.insertBefore(err, target.nextSibling);
-        }
-
-    }
-
-    submitHandler(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        if (this.validate()) {
-            this.isValid = true;
+            this.$errors.push(errors);
+            if (!target.classList.contains('invalid')) {
+                target.classList.add('invalid')
+            }
         }
     }
 
-
-
-    validate() {
-        let validated = true;
-
-
-
-        return validated;
+    if (!errors && this.$errors.length){
+        const noErr = this.$errors.find(item => item.name === target.name);
+        if (noErr) {
+            const filtered = this.$errors.filter(item => item.name !== target.name);
+            this.$errors = [...filtered];
+            if (target.classList.contains('invalid')) {
+                target.classList.remove('invalid')
+            }
+        }
     }
 
+      console.log(this.$errors);
+
+  }
+
+  submitHandler ( e ) {
+    e.preventDefault();
+    e.stopPropagation();
+    if ( this.validate() ) {
+      this.isValid = true;
+    }
+  }
+
+  validate () {
+    let validated = true;
+
+    return validated;
+  }
 
 }
